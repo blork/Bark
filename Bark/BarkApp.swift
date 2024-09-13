0a7be592badc026482454904931c1637c2934667
Sam Oakley <sam@blork.co.uk>
2024-09-13 15:08:36 +0100

    Initial commit

---
+++ Bark/BarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarkApp.swift
+//  Bark
+//
+//  Created by Sam Oakley on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
